c602566f4f61cbcd5021db93fd55dceab87b238d
Roshni4 <roshni-shah@hotmail.com>
2020-03-26 17:39:15 +0000

    Update tst_sortasc.cpp

--- sortingAlgorithm/tst_sortasc.cpp
+++ sortingAlgorithm/tst_sortasc.cpp
@@ -13,7 +13,8 @@ public:
 private slots:
    // void test_case1();
     //void test_case2();
-    void test_case3();
+    //void test_case3();
+    void test_case4();
 
 };
 
@@ -89,7 +90,7 @@ void sortAscDes::test_case2()//Sorting for descending classifier names
     //QCOMPARE(returnList,list2);
     QCOMPARE(returnList,list3);
 }*/
-void sortAscDes::test_case3() //Sorting for ascending image data names
+/*void sortAscDes::test_case3() //Sorting for ascending image data names
 {
     bool swapped = false;
     std::vector<std::string> names = {"lava_abstract","ark-survival-evolved","snakegame","dragon_eye","toothless_night_fury_dragon_5k"};
@@ -115,12 +116,43 @@ void sortAscDes::test_case3() //Sorting for ascending image data names
             }
 
 
-    QStringList list5 = {"ark-survival-evolved","dragon_eye","lava_abstract","snakegame","toothless_night_fury_dragon_5k"};
-    QStringList list6 = {"dragon_eye","toothless_night_fury_dragon_5k","lava_abstract","ark-survival-evolved","snakegame"};
+    QStringList list5 = {"ark-survival-evolved","dragon_eye","lava_abstract","snakegame","toothless_night_fury_dragon_5k"};//In actual ascending order
+    QStringList list6 = {"dragon_eye","toothless_night_fury_dragon_5k","lava_abstract","ark-survival-evolved","snakegame"};// this is to show the function works
     QVERIFY(true);
     QCOMPARE(returnList,list6);
-}
+}*/
 
+void sortAscDes::test_case4()//Sorting for descending image data names
+{
+    bool swapped = false;
+    std::vector<std::string> names = {"lava_abstract","ark-survival-evolved","snakegame","dragon_eye","toothless_night_fury_dragon_5k"};
+        for (unsigned int i = 0; i < names.size() - 1; ++i) {
+            for (unsigned int j = 0; j < names.size() - 1 - i; ++j) {
+
+                if (names[j] < names[j + 1]) {
+                    std::string temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            swapped = false;
+        }
+        QStringList returnList;
+            for(unsigned i=0; i < names.size(); i++) {
+                QString item = QString::fromStdString(names[i]);
+                returnList.append(item);
+            }
+
+    QStringList list7 = {"toothless_night_fury_dragon_5k","snakegame","lava_abstract","dragon_eye","ark-survival-evolved"};//In order
+    QStringList list8= {"dragon_eye","toothless_night_fury_dragon_5k","lava_abstract","ark-survival-evolved","snakegame"};//not in order
+    QVERIFY(true);
+    //QCOMPARE(returnList,list7);
+    QCOMPARE(returnList,list8);
+}
 QTEST_APPLESS_MAIN(sortAscDes)
 
 #include "tst_sortasc.moc"
